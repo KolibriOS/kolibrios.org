4175c09010f2baa2190fa606a7122cc29c91809c
clevermouse <clevermouse@ae7b2cb0-843d-483b-9e9e-b8a90ea88d30>
2020-05-06 19:16:36 +0000

    initial commit
    git-svn-id: file:///var/kolibrios-org-repos@1 ae7b2cb0-843d-483b-9e9e-b8a90ea88d30

---
+++ script.js
@@ -0,0 +1,117 @@
+/* Screenshots galery */
+
+var FIRST_IMG_ID = 1;
+var LAST_IMG_ID = 6;
+var current = FIRST_IMG_ID;
+var previews_visible = true;
+
+function checkkey(e)
+{
+	var keycode = window.event ? e.keyCode : e.which;
+	if (keycode==37) { previous(); }
+	else if (keycode==39) { next(); }
+	else if (keycode==27) { dropdown_hide(); }
+}
+
+window.onload = function() {
+	/*
+	if (document.documentElement.clientWidth<800)
+	{
+		document.getElementById("show").style.top = (document.documentElement.clientHeight - document.getElementById("show").offsetHeight) /2 + "px";
+	}
+	*/
+	if (document.getElementById("show"))
+	if (document.getElementById("screen").offsetHeight <  800) setTimeout( 'previews();', 1000);
+}
+
+function next() {
+	document.getElementById("mslide"+current).className = "minislide";
+	document.getElementById("slide"+current).className = "minislide";
+	if (current >= LAST_IMG_ID) current = FIRST_IMG_ID; else current++;
+	document.getElementById("slide"+current).className = "visible";
+	document.getElementById("mslide"+current).className = "minislide active";
+	document.getElementById("slide_description").innerHTML = document.getElementById("slide"+current).alt;
+}
+function previous() {
+	document.getElementById("mslide"+current).className = "minislide";
+	document.getElementById("slide"+current).className = "minislide";
+	if (current <= FIRST_IMG_ID) current = LAST_IMG_ID; else current--;
+	document.getElementById("slide"+current).className = "visible";
+	document.getElementById("mslide"+current).className = "minislide active";
+	document.getElementById("slide_description").innerHTML = document.getElementById("slide"+current).alt;
+}
+function mini(new_current) {
+	document.getElementById("mslide"+current).className = "minislide";
+	document.getElementById("slide"+current).className = "minislide";
+	document.getElementById("slide"+new_current).className = "visible";
+	document.getElementById("mslide"+new_current).className = "minislide active";
+	document.getElementById("slide_description").innerHTML = document.getElementById("slide"+new_current).alt;
+	current = new_current;
+}
+
+function previews() {
+	if (previews_visible==true)
+	{
+		previews_visible=false;
+		document.getElementById("text_preview").innerHTML = "show previews";
+		hide_previews(0);
+	}
+	else
+	{
+		previews_visible = true;
+		document.getElementById("text_preview").innerHTML = "hide previews";
+		document.getElementById("panel_minislides").style.height = "auto";
+		document.getElementById("spoiler").style.borderBottom = "1px #555 solid";
+	}
+}
+
+function hide_previews(i) {
+	if (i<80) {
+		document.getElementById("panel_minislides").style.height = 80 - i + "px";
+		i+=3;
+		setTimeout( 'hide_previews('+i+')', 1);
+	}
+	else
+	{
+		document.getElementById("spoiler").style.borderBottom = "0";
+	}
+}
+
+
+/* DropDown */
+
+function dropdown_show(obj)
+{
+	var x = y = 0;
+	while(obj) {
+		x += obj.offsetLeft;
+		y += obj.offsetTop;
+		obj = obj.offsetParent;
+	}
+	ddown = document.getElementById("lang-dropdown");
+	ddown.style.display = "block";
+	ddown.style.left    = (x - 72) + "px";
+	if (ddown.offsetLeft + ddown.offsetWidth +10 > document.body.offsetWidth) 
+	{
+		ddown.style.left = document.body.offsetWidth - ddown.offsetWidth - 82 + "px";
+	}
+	ddown.style.top     = (y + 48) + "px";
+	op = 0;
+	appear(1);	
+}
+
+function dropdown_hide()
+{
+	ddown = document.getElementById("lang-dropdown");
+	ddown.style.display="none";
+}
+
+function appear(x)
+{
+	if(op < x) {
+		op += 0.2;
+		ddown.style.opacity = op;
+		ddown.style.filter='alpha(opacity='+op*100+')';
+		t=setTimeout('appear('+x+')',20);
+	}
+}
