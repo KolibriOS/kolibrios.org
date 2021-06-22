297fde7229e333291795273c6dfc83578f13b852
leency <leency@ae7b2cb0-843d-483b-9e9e-b8a90ea88d30>
2021-06-22 10:57:55 +0000

    website: no screenshot previews
    git-svn-id: file:///var/kolibrios-org-repos@23 ae7b2cb0-843d-483b-9e9e-b8a90ea88d30

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 
 var FIRST_IMG_ID = 1;
 var LAST_IMG_ID = 6;
-var current = FIRST_IMG_ID;
-var previews_visible = true;
+var current = LAST_IMG_ID;
+
+window.onload = function() {
+	next();
+}
 
 function checkkey(e)
 {
@@ -13,60 +16,17 @@ function checkkey(e)
 	else if (keycode==27) { dropdown_hide(); }
 }
 
-window.onload = function() {
-	if (document.getElementById("show"))
-	if (document.getElementById("screen").offsetHeight <  1200) setTimeout( 'toggle_previews();', 1000);
-}
-
 function next() {
-	document.getElementById("mslide"+current).className = "minislide";
 	document.getElementById("slide"+current).className = "minislide";
 	if (current >= LAST_IMG_ID) current = FIRST_IMG_ID; else current++;
 	document.getElementById("slide"+current).className = "visible";
-	document.getElementById("mslide"+current).className = "minislide active";
-	document.getElementById("spoiler").innerHTML = document.getElementById("slide"+current).alt;
+	document.getElementById("carousel").innerHTML = document.getElementById("slide"+current).alt;
 }
 function previous() {
-	document.getElementById("mslide"+current).className = "minislide";
 	document.getElementById("slide"+current).className = "minislide";
 	if (current <= FIRST_IMG_ID) current = LAST_IMG_ID; else current--;
 	document.getElementById("slide"+current).className = "visible";
-	document.getElementById("mslide"+current).className = "minislide active";
-	document.getElementById("spoiler").innerHTML = document.getElementById("slide"+current).alt;
-}
-function mini(new_current) {
-	document.getElementById("mslide"+current).className = "minislide";
-	document.getElementById("slide"+current).className = "minislide";
-	document.getElementById("slide"+new_current).className = "visible";
-	document.getElementById("mslide"+new_current).className = "minislide active";
-	document.getElementById("spoiler").innerHTML = document.getElementById("slide"+new_current).alt;
-	current = new_current;
-}
-
-function toggle_previews() {
-	if (previews_visible==true)
-	{
-		previews_visible=false;
-		hide_previews(0);
-	}
-	else
-	{
-		previews_visible = true;
-		document.getElementById("panel_minislides").style.height = "auto";
-		document.getElementById("spoiler").style.borderBottom = "1px #555 solid";
-	}
-}
-
-function hide_previews(i) {
-	if (i<80) {
-		document.getElementById("panel_minislides").style.height = 80 - i + "px";
-		i+=3;
-		setTimeout( 'hide_previews('+i+')', 1);
-	}
-	else
-	{
-		document.getElementById("spoiler").style.borderBottom = "0";
-	}
+	document.getElementById("carousel").innerHTML = document.getElementById("slide"+current).alt;
 }
 
 
