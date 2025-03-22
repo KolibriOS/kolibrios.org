fb0d1c21991e60e555acfbe3c39ff7c3879852f0
Sweetbread <risdeveau@codrs.ru>
2025-03-22 18:27:43 +0300

    wip: rewrite to flask

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, url_for
+from datetime import date
+from configparser import ConfigParser
+
+locale = ConfigParser()
+locale.read('locale.ini')
+app = Flask(__name__)
+
+print(locale)
+
+@app.route("/<lang>")
+def index(lang):
+    return render_template(f'{lang}.html', locale=locale, lang=lang, year=date.today().year)  # TODO: Check for vulnerability
+
+@app.route("/<lang>/download")
+def download_page(lang):
+    return render_template('tmpl/download.htm', locale=locale, lang=lang)
+
+app.run(host="0.0.0.0", debug=True)
