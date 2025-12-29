72bb6c2e79ecdaa2c0be59ed5b0d4c517a690b78
Burer <burer@kolibrios.org>
2025-12-29 21:20:56 +0200

    tweak: enable JS minification for non-debug mode

--- app.py
+++ app.py
@@ -14,20 +14,20 @@ app = Flask(__name__)
 
 locales.ensure_loaded()
 
+# CSS Compilation and minification
 if app.debug:
-    # CSS Compilation and minification
     css = compile_sass(filename="static/style.scss", output_style="compressed")
     with open("static/style.css", "w", encoding="utf-8") as f:
         f.write(css)
 
-    # JS minification
-    with open("static/script.js", encoding="utf-8") as f:
-        js = f.read()
-    js = re.sub(r"/\*.*?\*/", "", js, flags=re.S)
-    js = re.sub(r"//.*", "", js)
-    js = re.sub(r"\s+", " ", js).strip()
-    with open("static/script.min.js", "w", encoding="utf-8") as f:
-        f.write(js)
+# JS minification
+with open("static/script.js", encoding="utf-8") as f:
+    js = f.read()
+js = re.sub(r"/\*.*?\*/", "", js, flags=re.S)
+js = re.sub(r"//.*", "", js)
+js = re.sub(r"\s+", " ", js).strip()
+with open("static/script.min.js", "w", encoding="utf-8") as f:
+    f.write(js)
 
 
 @app.before_request
